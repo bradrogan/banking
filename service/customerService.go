c9e58bea4cd62e7feaea7777121b84ee7e0f5fde
Brad Rogan <brad.rogan@gmail.com>
2022-11-20 23:20:41 -0700

    Refactor CustomerService

--- service/customerService.go
+++ service/customerService.go
@@ -5,23 +5,18 @@ import (
 	"github.com/bradrogan/banking/errs"
 )
 
-type CustomerService interface {
-	GetAllCustomers() ([]domain.Customer, error)
-	GetCustomer(string) (*domain.Customer, *errs.AppError)
-}
-
-type DefaultCustomerService struct {
+type CustomerService struct {
 	repo domain.CustomerRepository
 }
 
-func (s DefaultCustomerService) GetAllCustomers() ([]domain.Customer, error) {
+func (s CustomerService) GetAllCustomers() ([]domain.Customer, error) {
 	return s.repo.FindAll()
 }
 
-func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
+func (s CustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
 	return s.repo.ById(id)
 }
 
-func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService {
-	return DefaultCustomerService{repo: repository}
+func NewCustomerService(repository domain.CustomerRepository) CustomerService {
+	return CustomerService{repo: repository}
 }
