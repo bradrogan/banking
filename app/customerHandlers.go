32db2f30161532022cc23a234ba79432f3823e96
Brad Rogan <brad.rogan@gmail.com>
2022-12-12 00:19:28 -0700

    customerHander test

--- app/customerHandlers.go
+++ app/customerHandlers.go
@@ -9,7 +9,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-//go:generate mockgen -destination=../mocks/app/mockCustomerServicer.go -package=app github.com/bradrogan/banking/app CustomerServicer
+//go:generate mockgen -destination=../mock/mockCustomerServicer.go -package=mock github.com/bradrogan/banking/app CustomerServicer
 type CustomerServicer interface {
 	GetAllCustomers() ([]dto.CustomerResponse, *errs.AppError)
 	GetCustomersByStatus(customer.Status) ([]dto.CustomerResponse, *errs.AppError)
--- app/customerHandlers_test.go
+++ app/customerHandlers_test.go
@@ -2,7 +2,13 @@ package app
 
 import (
 	"net/http"
+	"net/http/httptest"
+	"reflect"
 	"testing"
+
+	"github.com/bradrogan/banking/errs"
+	"github.com/bradrogan/banking/mock"
+	"github.com/golang/mock/gomock"
 )
 
 func TestCustomerHandlers_getAllCustomers(t *testing.T) {
@@ -11,15 +17,41 @@ func TestCustomerHandlers_getAllCustomers(t *testing.T) {
 		r *http.Request
 	}
 	tests := []struct {
-		name string
-		ch   *CustomerHandlers
-		args args
+		name           string
+		prepare        func(m *mock.MockCustomerServicer)
+		args           args
+		wantStatusCode int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test get all customers",
+			prepare: func(m *mock.MockCustomerServicer) {
+				m.EXPECT().GetAllCustomers().Return(nil, errs.NewNotFoundError("dummyErr"))
+			},
+			args: args{
+				w: httptest.NewRecorder(),
+				r: httptest.NewRequest(http.MethodGet, "/customers", nil),
+			},
+			wantStatusCode: http.StatusNotFound,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tt.ch.getAllCustomers(tt.args.w, tt.args.r)
+			ctrl := gomock.NewController(t)
+			service := mock.NewMockCustomerServicer(ctrl)
+			if tt.prepare != nil {
+				tt.prepare(service)
+			}
+			ch := &CustomerHandlers{
+				service: service,
+			}
+			ch.getAllCustomers(tt.args.w, tt.args.r)
+
+			got := tt.args.w.(*httptest.ResponseRecorder).Result().StatusCode
+
+			if !reflect.DeepEqual(got, tt.wantStatusCode) {
+				t.Errorf("CustomerHandlers.getallCustomers() = %v, want %v", got, tt.wantStatusCode)
+			}
+
 		})
 	}
 }
